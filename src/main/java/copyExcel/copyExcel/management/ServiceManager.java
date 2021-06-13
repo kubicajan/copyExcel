653b22d4bf9826ae7561acbeea2ff54a2a032bc0
Jan Kubica <kubica.j88@gmail.com>
2021-06-13 20:14:25 +0200

    logback implemented

--- src/main/java/copyExcel/copyExcel/management/ServiceManager.java
+++ src/main/java/copyExcel/copyExcel/management/ServiceManager.java
@@ -5,10 +5,13 @@ import copyExcel.copyExcel.models.SheetSpecifics;
 import copyExcel.copyExcel.services.ReadFromExcel;
 import copyExcel.copyExcel.services.WriteToExcel;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -19,6 +22,7 @@ public class ServiceManager {
     private final ReadFromExcel readFromExcel;
 
     private final WriteToExcel writeToExcel;
+
 
     @EventListener(ApplicationReadyEvent.class)
     public void process() {
