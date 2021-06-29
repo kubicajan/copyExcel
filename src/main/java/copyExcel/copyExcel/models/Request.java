696994cfaa168ef8a3a70ef791b432f7067d4455
Jan Kubica <kubica.j88@gmail.com>
2021-06-29 23:25:34 +0200

    Request model added

---
+++ src/main/java/copyExcel/copyExcel/models/Request.java
@@ -0,0 +1,27 @@
+package copyExcel.copyExcel.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+public class Request {
+
+
+    //sourceFileSpecification
+    private String sourceFileName;
+    private List<Coordinate> readFromCoordinates;
+    private Set<String> readFromSheets;
+
+    //destinationFileSpecification
+    private String destinationFileName;
+    private Coordinate transposedCoordinateToStartWritingAt;
+    private Coordinate regularCoordinateToStartWritingAt;
+    private Set<String> transposedSheetNamesToWriteTo;
+    private Set<String> regularSheetsNamesToWriteTo;
+
+
+}
