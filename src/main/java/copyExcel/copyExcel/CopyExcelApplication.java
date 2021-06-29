effdc5f4b2172b0ceadda3db93ab8a23d408b0c2
Jan Kubica <kubica.j88@gmail.com>
2021-06-29 23:29:12 +0200

    Configuration loaded from json

--- src/main/java/copyExcel/copyExcel/CopyExcelApplication.java
+++ src/main/java/copyExcel/copyExcel/CopyExcelApplication.java
@@ -1,27 +1,50 @@
 package copyExcel.copyExcel;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import copyExcel.copyExcel.management.ServiceManager;
+import copyExcel.copyExcel.models.Request;
 import copyExcel.copyExcel.services.ReadFromExcel;
 import copyExcel.copyExcel.services.WriteToExcel;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 public class CopyExcelApplication {
     public static void main(String[] args) {
-        ServiceManager serviceManager = new ServiceManager(new ReadFromExcel(), new WriteToExcel());
-        ServiceManager serviceManager2 = new ServiceManager(new ReadFromExcel(), new WriteToExcel());
-
+        List<Request> requests = loadConfigurations();
         //todo: sometimes a bug happens: https://stackoverflow.com/questions/64116589/error-occurs-while-saving-the-package-the-part-xl-sharedstrings-xml-fail-to-b
 
-        //todo: check if everything gets loaded correctly.
-        
-        //to catch runtime exceptions and log them
-        try {
-            new Thread(serviceManager::firstFilePair).start();
-            new Thread(serviceManager2::secondFilePair).start();
-
-        } catch (Exception e) {
-            log.error("Something went wrong.", e);
+        if (!requests.isEmpty()) {
+            for (Request request : requests) {
+                startThread(request);
+            }
+            return;
         }
+        log.warn("No configurations were found.");
+    }
+
+    private static void startThread(Request request) {
+
+    }
+
+    private static List<Request> loadConfigurations() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Request> requests = new ArrayList<>();
+
+        try {
+            requests = objectMapper.readValue(
+                    new File("data.json"),
+                    new TypeReference<>() {
+                    });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return requests;
     }
 }
